6ac82be6b2b377395432cf285b1927bee1710fb1
sundram <ram322222@gmail.com>
2026-03-23 13:20:06 +0530

    created middleware fir auth,error handeling & rbac

---
+++ FinFlow-Node/src/middleware/auth.middleware.ts
@@ -0,0 +1,31 @@
+import { Response, NextFunction } from 'express'
+import { verifyAccessToken } from '../utils/jwt.utils'
+import { sendError } from '../utils/apiResponse'
+import { AuthRequest } from '../types'
+
+export const authenticate = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+): void => {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader?.startsWith('Bearer ')) {
+    sendError(res, 'No token provided', 401)
+    return
+  }
+
+  const token = authHeader.split(' ')[1]
+
+  try {
+    const payload = verifyAccessToken(token)
+    req.user = payload
+    next()
+  } catch (err: any) {
+    if (err.name === 'TokenExpiredError') {
+      sendError(res, 'Token expired', 401, 'TOKEN_EXPIRED')
+      return
+    }
+    sendError(res, 'Invalid token', 401, 'INVALID_TOKEN')
+  }
+}
